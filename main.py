f6d22e50086f603adaf881ca4a1ceb4f4f16eb7f
Sabbha20 <sabbha.power1@gmail.com>
2024-09-16 11:41:42 +0530

    get_gemini_response created

--- main.py
+++ main.py
@@ -6,5 +6,12 @@ import os
 from PIL import Image
 import google.generativeai as genai
 
-genai.configure(api_key="GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+# load model
+model = genai.GenerativeModel("gemini-pro-vision")
+
+def get_gemini_response(msg, img, user_prompt):
+    res = model.generate_content(msg, img[0], user_prompt)
+    return res
 
